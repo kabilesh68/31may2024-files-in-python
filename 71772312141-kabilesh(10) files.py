41e99240b786ac28f94997db9471b681d2a9c29c
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-31 11:42:31 +0530

    Update 71772312141-kabilesh(10) files.py

--- 71772312141-kabilesh(10) files.py
+++ 71772312141-kabilesh(10) files.py
@@ -6,4 +6,13 @@ data = data.replace(' an ', ' ')
 data = data.replace(' the ', ' ')
 f = open('b.txt', 'w')
 f.write(data)
-f.close( )
+f.close( )
+output:
+The input file 'a.txt' contains following text:
+The world is full of duplicates.
+I want an apple a day.
+I cannot do the stuff that you want me to do.
+The output file 'b.txt' contains following text:
+The world is full of duplicates.
+I want apple day.
+I cannot do stuff that you want me to do
