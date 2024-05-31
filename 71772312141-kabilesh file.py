7dd333bee52c7357784b48a24044d2c0a33ea7e6
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-31 11:11:07 +0530

    Update 71772312141-kabilesh file.py

--- 71772312141-kabilesh file.py
+++ 71772312141-kabilesh file.py
@@ -5,3 +5,8 @@ while True :
     break
   print(data)
 f.close( )
+output:
+CPython - is the reference implementation, written in C.
+PyPy - Written in a subset of Python language called RPython.
+Jython - Written in Java.
+IronPython - Written in C#.
