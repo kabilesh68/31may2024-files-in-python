7e01981631108b4bb19d91abad3498edbfdbb192
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-31 11:34:46 +0530

    Update 71772312141-kabilesh(5) files.py

--- 71772312141-kabilesh(5) files.py
+++ 71772312141-kabilesh(5) files.py
@@ -25,3 +25,31 @@ if data2 != '' :
 f1.close( )
 f2.close( )
 f3.close( )
+output:
+File 'sample.txt' contains following lines:
+1. SANJAY 25
+2. SAMEER 30
+3. ANIL 23
+4. SURESH 33
+5. PRABHU 22
+6. DINESH 40
+7. Suresh 34
+File 'trial.txt' contains following lines:
+1. Sandhya 25
+2. Seema 30
+3. Swati 23
+4. Supriya 33
+5. Sunidhi 22
+Resulting file 'comined.txt' contains following lines:
+1. SANJAY 25
+1. Sandhya 25
+2. SAMEER 30
+2. Seema 30
+3. ANIL 23
+3. Swati 23
+4. SURESH 33
+4. Supriya 33
+5. PRABHU 22
+5. Sunidhi 22
+6. DINESH 40
+7. Suresh 34
