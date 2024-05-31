680314892acdf6b9e9da15d752e5697844cb8dec
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-31 11:12:37 +0530

    Update 71772312141-kabilesh(2) files.py

--- 71772312141-kabilesh(2) files.py
+++ 71772312141-kabilesh(2) files.py
@@ -18,3 +18,16 @@ f2.seek(0, 0)
 f2.write(para2)
 f1.close( )
 f2.close( )
+output:
+'sample.txt' contains a few lines in lower case. 'trial.txt. contains the
+same lines in uppercase. After concatenation the contents of 'tiral.txt' is
+as shown below:
+CPYTHON - IS THE REFERENCE IMPLEMENTATION, WRITTEN
+IN C. PYPY - WRITTEN IN A SUBSET OF PYTHON LANGUAGE
+CALLED RPYTHON.
+JYTHON - WRITTEN IN JAVA.
+IRONPYTHON - WRITTEN IN C#.
+CPython - is the reference implementation, written in C.
+PyPy - Written in a subset of Python language called RPython.
+Jython - Written in Java.
+IronPython - Written in C#
